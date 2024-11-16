5be1696bbfe2916eaed15e629577c0b2d6346203
KuptsovAleksandr <kuptsov.dev@gmail.com>
2024-11-16 14:26:40 +0500

    created dev server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(
   options: BuildOptions
@@ -23,5 +24,7 @@ export function buildWebpackConfig(
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: "inline-source-map",
+    devServer: buildDevServer(options),
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import { describe } from "node:test";
 import { someFn } from "./test";
 
 someFn(123);
+
+document.body.innerHTML = `<div>Дароу</div>`;
